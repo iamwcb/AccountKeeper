90216d38f40378c5e1d3306200da1e3c7c04b475
iamwcb <willem.wucb@gmail.com>
2021-10-25 23:07:00 +0800

    修改图标；增加密码记忆功能；在user.ini中存储密码

--- Resource/pic2py.py
+++ Resource/pic2py.py
@@ -24,6 +24,6 @@ def pic2py(picture_names, py_name):
 
 
 if __name__ == '__main__':
-    pics = ["modify.png"]
+    pics = ["akico.ico"]
     pic2py(pics, 'memory_pic')  # 将pics里面的图片写到 memory_pic.py 中
     print("ok")
